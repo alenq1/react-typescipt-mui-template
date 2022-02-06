0b146b2d0aa860003cee9443e6a3ec287885cd3f
alenq1 <alejandro.castellanos@payco.co>
2022-02-06 13:40:53 -0400

    initial project structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { store } from './app/store';
+import App from './components/App';
+import { store } from './store/store';
 import { Provider } from 'react-redux';
+import { ThemeProvider} from '@mui/material/styles'
+import { theme } from "./theme/default";
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
-    </Provider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+      </BrowserRouter>
+    </Provider>    
   </React.StrictMode>,
   document.getElementById('root')
 );
